f442e85b2175292c81076e408023a8b489b3e012
saschahuettner <sascha.huettner@gmail.com>
2020-07-14 16:12:02 +0300

    two quotes without new line

--- get-quote.py
+++ get-quote.py
@@ -2,14 +2,19 @@ def primary():
   # print("Keep it logically awesome.")
   import random
 
-  last = 13
-  rnd = random.randint(0, last)
-
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[rnd])
+  last = len(quotes) - 1
+  rnd1 = random.randint(0, last)
+  rnd2 = random.randint(0, last)
+  while rnd2 == rnd1:
+    rnd2 = random.randint(0, last)
+
+
+  print(quotes[rnd1].replace("\n", ""))
+  print(quotes[rnd2].replace("\n", ""))
 
 if __name__== "__main__":
   primary()
